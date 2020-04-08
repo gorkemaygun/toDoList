36d740ae7e48b66f7d14af4b4324fa210891f499
gorkemaygun <gorkemaygun94@gmail.com>
2020-04-07 20:14:07 -0400

    changes

--- toDoList.js
+++ toDoList.js
@@ -1,4 +1,5 @@
-var addBtn= document.querySelector('#addToDo');
+//JAVASCRIPT
+/*var addBtn= document.querySelector('#addToDo');
 addBtn.addEventListener('click',function(event){
 	var inputText=document.querySelector('input[name="addToDo"]').value;
 	var list=document.querySelector('ul');
@@ -18,3 +19,28 @@ addBtn.addEventListener('click',function(event){
 			itemToRemove.remove(this);
 		},1000)		
 	};
+*/
+
+//jQUERY
+$('#addToDo').click(function(){// click function for button
+	var newLi=$('#list').append($('<li>',{text:$('.input').val()
+	}));//Added new list item 
+	$('input[name="addToDo"]').val('');// cleaned input field
+
+	$("li").click(function() { // This function provides us when you click new-added list item ,the item removes from list after 1 second .
+     	var self=this;
+      		setTimeout(function() {
+      		
+        	$(self).remove();}, 1000);
+
+  });
+});
+
+$("li").click(function() { // This function provides us when you click list item ,the item removes from list after 1 second .
+      var self=this;
+      setTimeout(function() {
+      	
+        $(self).remove();}, 1000);
+
+  });
+
